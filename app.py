12b4ccf078628fce8941bcd586e817276b667c1d
amank15 <78678079+amank15@users.noreply.github.com>
2021-11-23 12:07:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import sklearn,joblib
+import sklearn
+import joblib
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
